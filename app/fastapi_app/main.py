f86265d14776a24686f7ef7b6f03bb0eaf251983
Носов Сергей Олегович <SONosov@datafort.ru>
2024-09-01 22:53:53 +0300

    Update

--- app/fastapi_app/main.py
+++ app/fastapi_app/main.py
@@ -4,8 +4,10 @@ from pprint import pformat
 
 import uvicorn
 from aiokafka import AIOKafkaProducer
+from clickhouse_driver import Client
 from fastapi import FastAPI
 
+from app.clickhouse.sql import clickhouse_init_sql_queries
 from app.fastapi_app.api.api_router import api_router
 from app.fastapi_app.settings.config import settings
 from app.fastapi_app.settings.logs import logger
@@ -15,9 +17,18 @@ from app.kafka.producers import kafka_producer
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     try:
+        # Kafka
         bootstrap = {'bootstrap_servers': '{}:{}'.format(settings.KAFKA_HOST, settings.KAFKA_PORT)}
         kafka_producer.aio_producer = AIOKafkaProducer(**bootstrap)
         await kafka_producer.aio_producer.start()  # type: ignore
+
+        # Clickhouse
+        clickhouse_client = Client.from_url(settings.CLICKHOUSE_DSN)
+        try:
+            for sql in clickhouse_init_sql_queries:
+                clickhouse_client.execute(sql.format(kafka_host=settings.KAFKA_HOST, kafka_port=settings.KAFKA_PORT))
+        finally:
+            clickhouse_client.disconnect()
         yield
     finally:
         await kafka_producer.aio_producer.stop()  # type: ignore
